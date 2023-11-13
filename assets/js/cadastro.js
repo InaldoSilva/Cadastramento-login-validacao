00ac83de716673c2d6dd1612e38c5089119373fe
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-11-13 16:07:09 -0300

    validação de value.length

---
+++ assets/js/cadastro.js
@@ -0,0 +1,75 @@
+// Operação de cadastro de usuário
+
+let user = document.querySelector('#user')
+let labelUser = document.querySelector('#labelUser')
+
+let email = document.querySelector('#email')
+let labelEmail = document.querySelector('#labelEmail')
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let confirmarSenha = document.querySelector('#confirmarSenha')
+let labelConfirmarSenha = document.querySelector('#labelConfirmarSenha')
+
+// validação value.length user
+
+user.addEventListener('keyup', ()=> {
+    if (user.value.length <= 4) {
+        labelUser.setAttribute('style', 'background-color: red')
+        labelUser.innerHTML = 'Insira no mínimo 5 caracteres.'
+        user.setAttribute('style', 'border-color: red')
+    } else {
+        labelUser.setAttribute('style', 'background-color: green')
+        labelUser.innerHTML = 'User'
+        user.setAttribute('style', 'border-color: green')
+    }   
+})
+
+// validação value.length email 
+
+email.addEventListener("keyup", () => {
+  if (email.value.length <= 4) {
+    labelEmail.setAttribute("style", "background-color: red");
+    labelEmail.innerHTML = "Insira um e-mail válido.";
+    email.setAttribute("style", "border-color: red");
+  } else {
+    labelEmail.setAttribute("style", "background-color: green");
+    labelEmail.innerHTML = "E-mail";
+    email.setAttribute("style", "border-color: green");
+  }
+});
+
+//validação value.length senha
+
+senha.addEventListener("keyup", () => {
+  if (senha.value.length <= 7 ) {
+    labelSenha.setAttribute("style", "background-color: red")
+    labelSenha.innerHTML = "Insira no mínimo 8 caracteres."
+    senha.setAttribute("style", "border-color: red")
+  } else {
+    labelSenha.setAttribute("style", "background-color: green")
+    labelSenha.innerHTML = "Senha"
+    senha.setAttribute("style", "border-color: green")
+    
+  }
+})
+
+confirmarSenha.addEventListener("keyup", () => {
+  if (confirmarSenha.value.length <= 7) {
+    labelConfirmarSenha.setAttribute("style", "background-color: red")
+    labelConfirmarSenha.innerHTML = "Insira no mínimo 8 caracteres."
+    confirmarSenha.setAttribute("style", "border-color: red")
+  } else {
+    labelConfimarSenha.setAttribute("style", "background-color: green")
+    labelConfirmarSenha.innerHTML = "Senha"
+    confirmarSenha.setAttribute("style", "border-color: green")
+  }
+})
+
+
+
+function cadastrar () {
+
+    alert('Cadastro efetuado!')
+
+}
---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Show and hide password & confirm password
+let btn = document.getElementById('eye-senha')
+let btnConfirm = document.getElementById("eye-confirm");
+
+btn.addEventListener('click', ()=> {
+    let inputSenha = document.querySelector('#senha')
+
+    if(inputSenha.getAttribute('type') == 'password'){
+        inputSenha.setAttribute('type', 'text')
+    } else {
+        inputSenha.setAttribute('type', 'password')
+    }
+})
+
+
+btnConfirm.addEventListener("click", () => {
+  let inputConfirmarSenha = document.querySelector('#confirmar-senha')
+
+  if (inputConfirmarSenha.getAttribute('type') == 'password') {
+    inputConfirmarSenha.setAttribute('type', 'text')
+  } else {
+    inputConfirmarSenha.setAttribute('type', 'password')
+  }
+})
+/////////////////Show and hide - end//////////////////
+
